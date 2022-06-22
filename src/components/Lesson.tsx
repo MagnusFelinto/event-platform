710cb872641930e4ce65845ccdcfa164a0fbfc9b
Magnus Felinto <magnusfelinto@gmail.com>
2022-06-21 22:12:59 -0300

    lesson

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,6 +1,26 @@
+import { CheckCircle } from 'phosphor-react'
+
 export function Lesson() {
     return (
-        <h1>Header</h1>
+        <a href="#">
+            <span className="LessonData">
+                Terça • 22 de junho • 19h00
+            </span>
+            <div className="LessonInfo">
+                <header className="LessonInfoHeader">
+                    <span className="LessonInfoHeaderSpan1">
+                        <CheckCircle size={20} />
+                        Conteúdo liberado
+                    </span>
+                    <span className="LessonInfoHeaderSpan2">
+                        AO VIVO
+                    </span>
+                </header>
+                <strong className="LessonStrong">
+                    Abertura do evento Ignite labs
+                </strong>
+            </div>
+        </a>
     )
 
 }
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,7 +1,19 @@
+import { Lesson } from "./Lesson";
+
 export function Sidebar() {
     return (
         <aside className="asideSidebar">
-
+            <span className="spanSidebar">
+                Cronogramas de aulas
+            </span>
+            <div className="divLesson">
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+                <Lesson />
+            </div>
         </aside>
     )
 
